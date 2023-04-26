0375e8a60e7e7fc021bb62e699bb8b23173136db
krishkd16 <krishdihora16@gmail.com>
2023-04-26 20:22:27 +0530

    p

---
+++ app/src/main/java/com/complain/LoginsignupActivity.java
@@ -0,0 +1,14 @@
+package com.complain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginsignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginsignup);
+    }
+}
